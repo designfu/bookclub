9fc6d82cf17880bb4c7019c924e7de5911707e4a
Chris Erbach <chris@jadegrel.net>
2026-03-06 14:30:53 -0600

    Craft: Jump react(-dom) to v17, react-dnd to 14, and react-redux to v8. Drop @material-ui/types. Patch for compatibility.

--- client/source/js/lib/reorderable-lists/ReorderableListItem.tsx
+++ client/source/js/lib/reorderable-lists/ReorderableListItem.tsx
@@ -1,106 +1,84 @@
-import * as React from 'react'
-import { findDOMNode } from 'react-dom'
-import { DragSource, DropTarget } from 'react-dnd'
-import ListItem from '@material-ui/core/ListItem'
+import * as React from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import ListItem from '@material-ui/core/ListItem';
 
 const style = {
   cursor: 'move',
 };
 
-const cardSource = {
-  beginDrag(props) {
-    return {
-      id: props.key,
-      index: props.index,
-    };
-  },
-};
+const LIST_ITEM_TYPE = 'listItem';
 
-const cardTarget = {
-  hover(props, monitor, component) {
-    const dragIndex = monitor.getItem().index;
-    const hoverIndex = props.index;
-
-    // Don't replace items with themselves
-    if (dragIndex === hoverIndex) {
-      return;
-    }
-
-    // Determine rectangle on screen
-    const hoverBoundingRect = (findDOMNode(component) as any).getBoundingClientRect();
-
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-    // Determine mouse position
-    const clientOffset = monitor.getClientOffset();
-
-    // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-    // Only perform the move when the mouse has crossed half of the items height
-    // When dragging downwards, only move when the cursor is below 50%
-    // When dragging upwards, only move when the cursor is above 50%
-
-    // Dragging downwards
-    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-      return;
-    }
-
-    // Dragging upwards
-    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-      return;
-    }
-
-    // Time to actually perform the action
-    props.moveListItem(dragIndex, hoverIndex);
-
-    // Note: we're mutating the monitor item here!
-    // Generally it's better to avoid mutations,
-    // but it's good here for the sake of performance
-    // to avoid expensive index searches.
-    monitor.getItem().index = hoverIndex;
-  },
-};
-
-const withDropTarget = DropTarget('listItem', cardTarget, connect => ({
-  connectDropTarget: connect.dropTarget(),
-}));
-
-const withDropSource = DragSource('listItem', cardSource, (connect, monitor) => ({
-  connectDragSource: connect.dragSource(),
-  isDragging: monitor.isDragging(),
-}));
-
-class ReorderableListItem extends React.Component<any, any> {
-  // static propTypes = {
-    // connectDragSource: PropTypes.func.isRequired,
-    // connectDropTarget: PropTypes.func.isRequired,
-    // handleOnClick: PropTypes.func.isRequired,
-    // index: PropTypes.number.isRequired,
-    // isDragging: PropTypes.bool.isRequired,
-    // id: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired]),
-    // text: PropTypes.string.isRequired,
-    // moveListItem: PropTypes.func.isRequired,
-  // };
-
-  render() {
-    const {
-      isDragging,
-      connectDragSource,
-      connectDropTarget,
-      // props for onClick selection
-      handleOnClick,
-    } = this.props;
-    const opacity = isDragging ? 0 : 1;
-
-    return connectDragSource(
-      connectDropTarget(
-        <div style={{...style, opacity}}>
-          <ListItem onClick={handleOnClick}>{this.props.children}</ListItem>
-        </div>)
-    );
-  }
+interface DragItem {
+  id: string | number;
+  index: number;
 }
 
-export default withDropTarget(withDropSource(ReorderableListItem));
+interface ReorderableListItemProps {
+  id: string | number;
+  index: number;
+  moveListItem: (dragIndex: number, hoverIndex: number) => void;
+  handleOnClick: () => void;
+  children: React.ReactNode;
+}
+
+const ReorderableListItem = (props: ReorderableListItemProps) => {
+  const ref = React.useRef<HTMLDivElement | null>(null);
+  const { id, index, moveListItem, handleOnClick, children } = props;
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: LIST_ITEM_TYPE,
+    item: { id, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }), [id, index]);
+
+  const [, drop] = useDrop<DragItem, void, unknown>(() => ({
+    accept: LIST_ITEM_TYPE,
+    hover(item, monitor) {
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      const node = ref.current;
+      if (!node) {
+        return;
+      }
+
+      const hoverBoundingRect = node.getBoundingClientRect();
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      if (!clientOffset) {
+        return;
+      }
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      moveListItem(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  }), [index, moveListItem]);
+
+  drag(drop(ref));
+
+  const opacity = isDragging ? 0 : 1;
+
+  return (
+    <div ref={ref} style={{ ...style, opacity }}>
+      <ListItem onClick={handleOnClick}>{children}</ListItem>
+    </div>
+  );
+};
+
+export default ReorderableListItem;
